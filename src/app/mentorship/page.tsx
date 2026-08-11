93672a58d62dc6db982dc493e8a57706cd5f8312
BeAnirva <beanirva@gmail.com>
2026-08-11 16:09:05 +0530

    Create page.tsx

---
+++ src/app/mentorship/page.tsx
@@ -0,0 +1,17 @@
+import Navbar from "@/components/Navbar";
+import Mentorship from "@/components/Mentorship";
+import Footer from "@/components/Footer";
+
+export default function MentorshipPage() {
+  return (
+    <main className="min-h-screen bg-[#F8F6F1]">
+
+      <Navbar />
+
+      <Mentorship />
+
+      <Footer />
+
+    </main>
+  );
+}
