4c64f2b680734f1f5670b1fa2d4ecf5c77a94db0
BeAnirva <beanirva@gmail.com>
2026-08-11 16:09:50 +0530

    Create page.tsx

---
+++ src/app/stories/page.tsx
@@ -0,0 +1,17 @@
+import Navbar from "@/components/Navbar";
+import Stories from "@/components/Stories";
+import Footer from "@/components/Footer";
+
+export default function StoriesPage() {
+  return (
+    <main className="min-h-screen bg-[#F8F6F1]">
+
+      <Navbar />
+
+      <Stories />
+
+      <Footer />
+
+    </main>
+  );
+}
