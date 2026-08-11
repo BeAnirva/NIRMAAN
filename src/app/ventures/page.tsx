99513ea42e6c9f3e7b07399a260d2b00e3069de8
BeAnirva <beanirva@gmail.com>
2026-08-11 16:08:13 +0530

    Create page.tsx

---
+++ src/app/ventures/page.tsx
@@ -0,0 +1,17 @@
+import Navbar from "@/components/Navbar";
+import Ventures from "@/components/Ventures";
+import Footer from "@/components/Footer";
+
+export default function VenturesPage() {
+  return (
+    <main className="min-h-screen bg-[#F8F6F1]">
+
+      <Navbar />
+
+      <Ventures />
+
+      <Footer />
+
+    </main>
+  );
+}
