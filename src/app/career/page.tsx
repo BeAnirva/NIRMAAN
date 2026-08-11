b11696e4cb4405f72228742f3a73754cac8767d1
BeAnirva <beanirva@gmail.com>
2026-08-11 16:07:37 +0530

    Create page.tsx

---
+++ src/app/career/page.tsx
@@ -0,0 +1,17 @@
+import Navbar from "@/components/Navbar";
+import Career from "@/components/Career";
+import Footer from "@/components/Footer";
+
+export default function CareerPage() {
+  return (
+    <main className="min-h-screen bg-[#F8F6F1]">
+
+      <Navbar />
+
+      <Career />
+
+      <Footer />
+
+    </main>
+  );
+}
