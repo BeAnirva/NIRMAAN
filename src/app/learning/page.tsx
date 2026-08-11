55ef661094fe043f0536898063817af16d3ab161
BeAnirva <beanirva@gmail.com>
2026-08-11 16:08:36 +0530

    Create page.tsx

---
+++ src/app/learning/page.tsx
@@ -0,0 +1,17 @@
+import Navbar from "@/components/Navbar";
+import Learning from "@/components/Learning";
+import Footer from "@/components/Footer";
+
+export default function LearningPage() {
+  return (
+    <main className="min-h-screen bg-[#F8F6F1]">
+
+      <Navbar />
+
+      <Learning />
+
+      <Footer />
+
+    </main>
+  );
+}
