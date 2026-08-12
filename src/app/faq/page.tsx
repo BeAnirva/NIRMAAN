52675f690f08a6e4f4a54d569d5ae2b3149be713
BeAnirva <beanirva@gmail.com>
2026-08-13 01:04:45 +0530

    Update page.tsx

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,1 +1,14 @@
+import Navbar from "@/components/Navbar";
+import FAQ from "@/components/FAQ";
 
+export default function FAQPage() {
+  return (
+    <main className="min-h-screen bg-[#F8F6F1]">
+
+      <Navbar />
+
+      <FAQ />
+
+    </main>
+  );
+}
