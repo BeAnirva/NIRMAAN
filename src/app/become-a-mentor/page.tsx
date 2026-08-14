62ae4b19d263e57af8bec96eb446a0c31a8ee7b5
BeAnirva <beanirva@gmail.com>
2026-08-14 21:08:48 +0530

    Update page.tsx

--- src/app/become-a-mentor/page.tsx
+++ src/app/become-a-mentor/page.tsx
@@ -44,18 +44,12 @@ export default function BecomeAMentor() {
                 Become a Mentor
               </p>
 
-              <h1 className="max-w-4xl text-5xl font-semibold leading-[0.95] tracking-[-0.055em] sm:text-6xl md:text-7xl lg:text-[5.5rem]">
-
+             <h1 className="max-w-4xl text-5xl font-semibold leading-[0.95] tracking-[-0.055em] sm:text-6xl md:text-7xl lg:text-[5.5rem]">
   Heroes don't always
   <br />
-
-  wear capes.
-  <br />
-
   <span className="font-serif italic font-normal">
-    They pass the spark forward.
+    wear capes.
   </span>
-
 </h1>
 
             </div>
