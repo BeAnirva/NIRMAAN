44348d4c79155809d8f1cd27336cbe1edf882ef7
BeAnirva <beanirva@gmail.com>
2026-08-18 22:23:20 +0530

    Update LanguageProvider.tsx

--- src/components/LanguageProvider.tsx
+++ src/components/LanguageProvider.tsx
@@ -1,1 +1,84 @@
+"use client";
 
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+
+import {
+  languages,
+  translations,
+  type Language,
+} from "@/lib/translations";
+
+type TranslationData = typeof translations.en;
+
+interface LanguageContextType {
+  language: Language;
+  setLanguage: (language: Language) => void;
+  t: TranslationData;
+}
+
+const LanguageContext = createContext<LanguageContextType | undefined>(
+  undefined
+);
+
+export function LanguageProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [language, setLanguageState] = useState<Language>("en");
+
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem(
+      "nirmaan-language"
+    ) as Language | null;
+
+    if (
+      savedLanguage &&
+      Object.prototype.hasOwnProperty.call(
+        languages,
+        savedLanguage
+      )
+    ) {
+      setLanguageState(savedLanguage);
+    }
+  }, []);
+
+  const setLanguage = (newLanguage: Language) => {
+    setLanguageState(newLanguage);
+
+    localStorage.setItem(
+      "nirmaan-language",
+      newLanguage
+    );
+  };
+
+  return (
+    <LanguageContext.Provider
+      value={{
+        language,
+        setLanguage,
+        t: translations[language],
+      }}
+    >
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+
+export function useLanguage() {
+  const context = useContext(LanguageContext);
+
+  if (!context) {
+    throw new Error(
+      "useLanguage must be used inside LanguageProvider"
+    );
+  }
+
+  return context;
+}
